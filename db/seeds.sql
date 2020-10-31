9cae25b0f4aa47fa8c9dccba5ce8661b64fe5b3c
Violet <70643274+violettaval@users.noreply.github.com>
2020-10-30 20:58:22 -0500

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('California');
+INSERT INTO burgers (name) VALUES ('Butter Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Baconator', true);
+INSERT INTO burgers (name, devoured) VALUES ('Juicy Lucy', true);
+INSERT INTO burgers (name, devoured) VALUES ('Five Guys', true);
+INSERT INTO burgers (name) VALUES ('Whopper');
