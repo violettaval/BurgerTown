0b2bba70884972aea985593e0a3fcce8491d88c9
Violet <70643274+violettaval@users.noreply.github.com>
2020-10-30 22:41:23 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,14 @@
-// Set up MySQL connection.
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "artist37",
-  database: "burger_db"
-});
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burger_db"
+  });
+};
+connection.connect();
 module.exports = connection;
